a656d6ebf5855376ac096b0a4292f67ed797205f
Manish <manishchaudhari1111@gmail.com>
2020-01-03 18:14:26 +0530

    Added changes for add to cart button

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 import SHOP_DATA from "./shop.data";
 
+import "./shop.styles.scss";
+
 class ShopPage extends React.Component {
 	constructor(props){
 		super(props);
@@ -14,9 +16,8 @@ class ShopPage extends React.Component {
 		return (
 			<div className="shop-data">
 				{
-          this.state.collections.map((collection) => <CollectionPreview key={collection.id} {...collection}/>)
-        }
-								
+					this.state.collections.map((collection) => <CollectionPreview key={collection.id} {...collection} />)
+				}				
 			</div>
 		)
 	}
